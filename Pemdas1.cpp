ec7bc0bb02dded293df50f47d1a8d69e7d38fd80
virgygasya-lgtm <virgygasya@gmail.com>
2026-02-23 12:30:56 +0700

    Pemdas1.cpp

---
+++ Pemdas1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+// Langkah 1 
+int luas, panjang, lebar;
+
+// Langkah 2
+cout << "Masukan panjang :"
+cin >> panjang;
+
+// Langkah 3
+cout << "Masukan lebar:"
+cin>> lebar;
+
+// Langkah 4
+luas = panjang * lebar;
+
+// Langkah 5 
+cout << ''Hasilnya : << luas;
+
+//selesai
